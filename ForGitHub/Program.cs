7f0b1f30a427548eb1579bf199b42246d7d7c097
asliddin abdurahmonov <asliddinabdurahmonov41@gmail.com>
2023-01-19 19:30:59 +0500

    project

---
+++ ForGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
